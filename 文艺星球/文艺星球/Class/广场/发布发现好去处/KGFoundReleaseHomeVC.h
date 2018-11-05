7a2c29e8a8d8d61b1f1275a574b018fb373125cd
bielian <myloveqzk@163.com>
2018-11-05 18:43:54 +0800

    发现好去处

---
+++ 文艺星球/文艺星球/Class/广场/发布发现好去处/KGFoundReleaseHomeVC.h
@@ -0,0 +1,17 @@
+//
+//  KGFoundReleaseHomeVC.h
+//  文艺星球
+//
+//  Created by 文艺星球 on 2018/11/5.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "KGBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KGFoundReleaseHomeVC : KGBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
